68c505d398ef6c1895f627bbf6cbdb47d1f0b3b6
MarkR3z <markruzal@mail.tau.ac.il>
2025-01-03 13:34:56 +0200

    some syntax changes

--- mulmat.asm
+++ mulmat.asm
@@ -23,22 +23,22 @@ add $s2, $zero, $imm1, $zero, 0x120, 0  # Result Matrix C
 # Loop through rows of A
 add $t0, $zero, $zero, $zero, 0, 0  # Row index i
 loop_i:
-    beq $t0, $imm1, $zero, $imm1, end, 4  # If i == 4, end
+    beq $zero, $imm2, $t0, $imm1, end, 4  # If i == 4, end
 
     # Loop through columns of B
     add $t1, $zero, $zero, $zero, 0, 0  # Column index j
     loop_j:
-        beq $t1, $imm1, $zero, $imm1, next_i, 4  # If j == 4, next row
+        beq $zero, $imm2, $t2, $imm1, next_i, 4  # If j == 4, next row
 
         # Calculate dot product for C[i][j]
         add $t2, $zero, $zero, $zero, 0, 0  # Sum
         add $t3, $zero, $zero, $zero, 0, 0  # Index k
         loop_k:
-            beq $t3, $imm1, $zero, $imm1, store, 4  # If k == 4, store result
+            beq $zero, $imm2, $t3, $imm1, store, 4  # If k == 4, store result
 
             # Load A[i][k] and B[k][j]
             mac $t0 , $t0, $imm1, $t3 ,4,0  # $t0 = offset for A[i][k]
-            lw $a0,$s0,$t0,$zero,0,0 #  $a0 = A[i][k]
+            lw $a0,$s0,$t0, $zero, 0, 0 #  $a0 = A[i][k]
             add $a1, $s1, $t3, $t1, 0, 0
             lw $a1, $a1, 0
 
@@ -52,7 +52,7 @@ loop_i:
         store:
         # Store result in C[i][j]
         add $a0, $s2, $t0, $t1, 0, 0
-        sw $a0, $t2, 0
+        sw $zero, $a0, $zero, $t2, 0, 0
 
         # Increment j
         add $t1, $t1, $imm1, $zero, 1, 0
